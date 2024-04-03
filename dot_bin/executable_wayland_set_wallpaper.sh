d99b41768e007fdf18e4c4208844d2d85ba0cb19
indisukka <125402205+indisukka@users.noreply.github.com>
2024-04-04 01:55:47 +0530

    updated configs

--- dot_bin/executable_wayland_set_wallpaper.sh
+++ dot_bin/executable_wayland_set_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 killall swaybg
-varFileName=$(find ~/Pictures/walls/ -type f | shuf -n 1)
+varFileName=$(find ~/Pictures/wallpapers/ -type f | shuf -n 1)
 # echo "$varFileName"
 swaybg -i "$varFileName" -m fill
